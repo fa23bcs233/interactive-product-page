527fadb8707d2b5d9128c765578c7ae4e28f71b9
fa23bcs233 <arhmraf@gmail.com>
2025-07-06 11:34:04 +0500

    reponsiveness layout added

--- slider.js
+++ slider.js
@@ -1,13 +1,24 @@
 class Slider{
-    constructor(sliderContainer, sliderTrack, prevButton, nextButton, controls){
-        this.sliderContainer = document.querySelector(sliderContainer);
+    constructor(slider,sliderContainer, sliderTrack, prevButton, nextButton, controls){
+        this.slider = document.querySelector(slider);
+        this.sliderContainer = this.slider.querySelector(sliderContainer);
+        this.prevButton = this.slider.querySelector(prevButton) || undefined;
+        this.nextButton = this.slider.querySelector(nextButton) || undefined;
+        this.controls = this.slider.querySelectorAll(controls) || undefined;
         this.sliderTrack = this.sliderContainer.querySelector(sliderTrack);
-        this.prevButton = document.querySelector(prevButton) || undefined;
-        this.nextButton = document.querySelector(nextButton) || undefined;
-        this.controls = document.querySelectorAll(controls) || undefined;
         this.currentSlideIndex = 0;
 
         this.init();
+    }
+
+
+    init(){
+        this.setSlideIndex(this.currentSlideIndex);
+        if(this.controls){
+            this.setControls();
+        }
+
+        this.initNavigation();
     }
 
     transformSlides(){
@@ -19,26 +30,52 @@ class Slider{
     setSlideIndex(index){
         this.currentSlideIndex = index;
         this.transformSlides();
+        this.activateControl();
+    }
+
+    activateControl(){
+        this.controls.forEach(ctrl=>{
+            ctrl.classList.remove("active");
+        })
+        this.controls[this.currentSlideIndex].classList.add("active");
     }
 
     setControls(){
         this.controls.forEach(control => {
             control.addEventListener("click", (e) => {
-                const slideIndex = e.currentTarget.getAttribute("data-refSlide");
-                this.controls.forEach(ctrl => ctrl.classList.remove("active"));
-                e.currentTarget.classList.add("active");
+                const slideIndex = parseInt(e.currentTarget.getAttribute("data-refSlide"));
                 this.setSlideIndex(slideIndex);
             }); 
         });
 
     }
-
-    init(){
-        this.setSlideIndex(this.currentSlideIndex);
-        if(this.controls){
-            this.setControls();
-        }
+    
+    initNavigation(){
+        this.prevButton?.addEventListener("click", () => {
+            if(this.currentSlideIndex > 0)
+                this.setSlideIndex(this.currentSlideIndex - 1);
+        });
+        this.nextButton?.addEventListener("click", () => {
+            if(this.currentSlideIndex < (this.controls.length - 1))
+                this.setSlideIndex(this.currentSlideIndex + 1);
+        });
     }
 }
 
-const slider = new Slider(".main-image-crousal", ".main-img-crousal-track", null, null, ".thumbnails-images-container .thumbnail-image");
+const slider = new Slider(".product-image-container" ,".main-image-crousal", ".main-img-crousal-track", ".prev-slide", ".next-slide", ".thumbnails-images-container .thumbnail-image");
+
+
+// Implementing the lightbox
+const lightBoxContainer = document.querySelector(".lightbox");
+const lightBox = lightBoxContainer.querySelector(".lightbox-crousal");
+const mainCrousal = document.querySelector(".product-image-container")
+lightBox.innerHTML = mainCrousal.innerHTML;
+new Slider(".lightbox-crousal", ".main-image-crousal", ".main-img-crousal-track", ".prev-slide", ".next-slide", ".thumbnails-images-container .thumbnail-image");
+
+function showLightBox(){
+    lightBoxContainer.style.display = "flex";
+}
+
+function hideLightBox(){
+    lightBoxContainer.style.display = "none";   
+}
